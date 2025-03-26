44e786c83c982bedd8a68aca426826f86ae3e6bb
Naveen Kumar Errojula <naveenkumarerroju@gmail.com>
2025-03-26 13:18:48 +0530

    Initial commit

---
+++ src/main/java/com/naveen/evcharging/charger_management/ChargerManagementApplication.java
@@ -0,0 +1,13 @@
+package com.naveen.evcharging.charger_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargerManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargerManagementApplication.class, args);
+	}
+
+}
