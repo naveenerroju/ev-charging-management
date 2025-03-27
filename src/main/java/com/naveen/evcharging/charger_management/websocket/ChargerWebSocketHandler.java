ef1cdca113783c3d33eb127192e16d6471b3720b
Naveen Kumar Errojula <naveenkumarerroju@gmail.com>
2025-03-27 19:01:50 +0530

    logs are implemented for connections and actions

--- src/main/java/com/naveen/evcharging/charger_management/websocket/ChargerWebSocketHandler.java
+++ src/main/java/com/naveen/evcharging/charger_management/websocket/ChargerWebSocketHandler.java
@@ -3,15 +3,23 @@ package com.naveen.evcharging.charger_management.websocket;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.naveen.evcharging.charger_management.service.ActionHandler;
+import com.naveen.evcharging.charger_management.util.DataExchangeUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
+@Slf4j
 public class ChargerWebSocketHandler extends TextWebSocketHandler {
+
+    private final Map<String, WebSocketSession> chargerSessions = new ConcurrentHashMap<>();
 
     private final ActionHandlerFactory actionHandlerFactory;
     private final ObjectMapper objectMapper = new ObjectMapper();
@@ -20,13 +28,14 @@ public class ChargerWebSocketHandler extends TextWebSocketHandler {
         this.actionHandlerFactory = actionHandlerFactory;
     }
 
-
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
-        String chargerId = getChargerIdFromPath(session);
+        String chargerId = DataExchangeUtil.getchargingStationIdFromPath(session);
         JsonNode root = objectMapper.readTree(message.getPayload());
 
         String action = root.path("action").asText();
+
+        log.info("New message received from {} for action: {}", chargerId, action);
 
         ActionHandler handler = actionHandlerFactory.getHandler(action);
 
@@ -38,8 +47,18 @@ public class ChargerWebSocketHandler extends TextWebSocketHandler {
         }
     }
 
-    private String getChargerIdFromPath(WebSocketSession session) {
-        String path = session.getUri().getPath(); // e.g., /ws/charger/CHARGER_001
-        return path.substring(path.lastIndexOf("/") + 1);
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        String chargerId = DataExchangeUtil.getchargingStationIdFromPath(session);
+        chargerSessions.put(chargerId, session);
+        log.info("New connection is established with {}", chargerId);
     }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status){
+        String chargerId = DataExchangeUtil.getchargingStationIdFromPath(session);
+        chargerSessions.put(chargerId, session);
+        log.info("Connection is closed with {}", chargerId);
+    }
+
 }
