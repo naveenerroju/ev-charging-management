eafd0bbd5e18decf6ff4747dfaf7ec05ed254200
Naveen Kumar Errojula <naveenkumarerroju@gmail.com>
2025-03-28 19:30:22 +0530

    transaction history rest api implemented

--- src/main/java/com/naveen/evcharging/charger_management/repository/ChargingTransactionRepository.java
+++ src/main/java/com/naveen/evcharging/charger_management/repository/ChargingTransactionRepository.java
@@ -4,6 +4,10 @@ import com.naveen.evcharging.charger_management.document.ChargingTransaction;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface ChargingTransactionRepository extends MongoRepository<ChargingTransaction, String> {
+    List<ChargingTransaction> findByStartTimeBetween(LocalDateTime startTime, LocalDateTime endTime);
 }
